24869573b8a65ff2ab882fbb7beedf40f211fcdd
Ryan Webster <97265322+Sail-Hatan-666@users.noreply.github.com>
2025-06-24 16:13:07 -0700

    Added TODOs to project.

--- 100_days_of_code/day_15/coffee_machine.py
+++ 100_days_of_code/day_15/coffee_machine.py
@@ -1,2 +1,24 @@
 import coffee_data
 
+run = True
+
+while run:
+    user_choice = input("What would you like to drink? (espresso/latte/cappuccino): \n").lower()
+
+    if user_choice == "off":
+        run = False
+
+    if user_choice == "report":
+        # TODO generate report showing current resource values
+        pass
+
+    # TODO check if resources are sufficient
+
+    # TODO process coine
+
+    # TODO check if transaction is successful
+        # if too much money, return change
+
+    # TODO Make the coffee
+
+    # Thank user for using coffemaker deluxe v10.0 
