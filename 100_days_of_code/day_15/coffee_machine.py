5d7eb1741b046e0937c304227c260f4126f3e384
Ryan Webster <97265322+Sail+Hatan-666@users.noreply.github.com>
2025-06-26 00:30:27 -0700

    Continued working on coffee_machine.py

--- 100_days_of_code/day_15/coffee_machine.py
+++ 100_days_of_code/day_15/coffee_machine.py
@@ -1,6 +1,9 @@
 import coffee_data
 
 resources = coffee_data.resources
+espresso = coffee_data.MENU["espresso"]
+latte = coffee_data.MENU["latte"]
+cappiccino = coffee_data.MENU["cappuccino"]
 
 def handle_report():
     report = (f"Water: {resources['water']}\n"
@@ -8,10 +11,14 @@ def handle_report():
     f"Coffee: {resources['coffee']}")
     return report
 
-def check_resources():
-    
-
-
+def check_resources(choice):
+    for ingredient in coffee_data.MENU[choice]["ingredients"]:
+        amount_needed = coffee_data.MENU[choice]['ingredients'][ingredient]
+        amount_left = coffee_data.resources[ingredient]
+        if amount_needed > amount_left:
+            print(f"Sorry we're all out {ingredient}")
+        else:
+            pass
 
 machine_on = True
 
@@ -28,8 +35,10 @@ while machine_on:
 
     # TODO Thank user for using coffemaker deluxe v10.0
 
+    if user_choice == "off":
+        machine_on = False
+
     if user_choice == "report":
         print(handle_report())
 
-    if user_choice == "off":
-        machine_on = False
+    check_resources(user_choice)
