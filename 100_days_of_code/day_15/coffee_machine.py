06c4b42671b6969cb1b1b22efef64f665f7a3377
Ryan Webster <97265322+Sail-Hatan-666@users.noreply.github.com>
2025-06-27 12:34:09 -0700

    Getting closer to the finish and further as well.

--- 100_days_of_code/day_15/coffee_machine.py
+++ 100_days_of_code/day_15/coffee_machine.py
@@ -1,10 +1,8 @@
 import coffee_data
 
 resources = coffee_data.resources
-espresso = coffee_data.MENU["espresso"]
-latte = coffee_data.MENU["latte"]
-cappiccino = coffee_data.MENU["cappuccino"]
 MENU = coffee_data.MENU
+
 
 def handle_report():
     report = (f"Water: {resources['water']}\n"
@@ -12,10 +10,10 @@ def handle_report():
     f"Coffee: {resources['coffee']}")
     return report
 
+
 # TODO check if resources are sufficient
 def check_resources(choice):
     missing_ingredients = []
-
     for ingredient in coffee_data.MENU[choice]["ingredients"]:
         amount_needed = coffee_data.MENU[choice]['ingredients'][ingredient]
         amount_left = coffee_data.resources[ingredient]
@@ -26,12 +24,26 @@ def check_resources(choice):
         missing_ingredients_formatted = ", ".join(missing_ingredients[:-1]) + " and " + missing_ingredients[-1]
         return f"Sorry, we're all out of {missing_ingredients_formatted}."
     else:
-        return handle_choice(choice)
+        return make_coffee(choice)
+
 
 # TODO Make the coffee
-def handle_choice(choice):
-    if choice in MENU:
-        print("Good Choice")
+def make_coffee(choice):
+    ingredients = [ingredient for ingredient in MENU[choice]["ingredients"]]
+    for ingredient in MENU[choice]["ingredients"]:
+        if ingredient in resources:
+            ingredients.append(ingredient)
+    return ingredients
+        
+
+
+
+    # When the user chooses their drink, deduct rources needed from current resources in coffee_data.py
+    # How do I do that?
+    # resources available - resources needed
+    
+    # resources - choice[ingredients]
+
 
 # TODO process coins
 def handle_payment():
@@ -39,21 +51,24 @@ def handle_payment():
     # TODO check if transaction is successful, if too much money, return change
 
 
-
 machine_on = True
 
 while machine_on:
-    user_choice = input("What would you like to drink? (espresso/latte/cappuccino): \n").lower()
+    user_choice = input(
+        "What would you like to drink? (espresso/latte/cappuccino): \n").lower()
 
     # TODO Thank user for using coffemaker deluxe v10.0
 
 
     # TODO fix error with user_choice = off caused by check_resources
-    if user_choice == "off":
-        machine_on = False
+    # if user_choice == "off":
+    #     machine_on = False
 
-    if user_choice == "report":
-        print(handle_report())
+    # if user_choice == "report":
+    #     print(handle_report())
 
     result = check_resources(user_choice)
-    print(result)
+    done = make_coffee(user_choice)
+
+    # print(result)
+    print(done)
