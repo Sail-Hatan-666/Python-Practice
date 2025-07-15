c974b596e4ad4940ce81a67b499b8d6d314ded02
Ryan Webster <97265322+Sail-Hatan-666@users.noreply.github.com>
2025-07-15 16:17:03 -0700

    Here I go breaking things again

--- 100_days_of_code/day_15/coffee_machine.py
+++ 100_days_of_code/day_15/coffee_machine.py
@@ -3,14 +3,13 @@ from coffee_data import resources
 from coffee_data import profit
 
 
-# [x] TODO Handle Report
 def handle_report():
     report = (f"Water: {resources['water']}\n"
                 f"Milk: {resources['milk']}\n"
                 f"Coffee: {resources['coffee']}")
     return report
 
-# [x] TODO Check Resources
+
 def check_resources(choice):
     missing_ingredients = []
     for ingredient in MENU[choice]["ingredients"]:
@@ -24,7 +23,6 @@ def check_resources(choice):
         return False, missing_ingredients
 
 
-# [x] TODO Make the coffee
 def make_coffee(choice):
     for key in MENU[choice]["ingredients"]:
         if key in resources:
@@ -32,7 +30,6 @@ def make_coffee(choice):
             resources[key] = updated_value
 
 
-# [x] TODO process coins
 def handle_payment(
         choice, 
         quarters, 
@@ -51,6 +48,18 @@ def handle_payment(
         return True, total, total_format, change
     elif total == cost:
         return True, total, total_format, change
+
+def input_validation(payment):
+    repeat = True
+    while repeat:
+        try:
+            payment
+        except ValueError:
+            print("Please enter a valid number")
+        else:
+            break
+        finally:
+            repeat = False
 
 
 machine_on = True
@@ -71,37 +80,75 @@ while machine_on:
 
     if user_choice in MENU:
         state, missing_ingredients = check_resources(user_choice)
-        pennies = int(input("How many pennies? \n")) * 0.01
-        nickels = int(input("How many nickles? \n")) * 0.05
-        dimes = int(input("How many dimes? \n")) * 0.1
-        quarters = int(input("How many quarters? \n")) * 0.25
-        money_in, total, total_format, change = handle_payment(
-                                                user_choice,
-                                                quarters,
-                                                dimes,
-                                                nickels,
-                                                pennies)
-        if money_in and total == MENU[user_choice]["cost"]:
-            make_coffee(user_choice)
-            print(f"You inserted {total_format}. "
-                    f"Your {user_choice.title()} is ready! Enjoy!")
-        elif money_in and total > MENU[user_choice]["cost"]:
-            make_coffee(user_choice)
-            print(f"You inserted {total_format}. "
-                    f"You're {user_choice.title()} is ready! Enjoy!\n"
-                    f"Here's your change: ${change}")
-        elif not money_in:
-            print(f"You inserted {total_format}. "
-                f"Sorry, {user_choice.title()} "
-                f"costs ${MENU[user_choice]['cost']:.2f}\n"
-                "Please try again")
-    elif user_choice in MENU and not state:
-        if len(missing_ingredients) > 1:
-            format_missing_ingredents = ", ".join(missing_ingredients[:-1])
-            + " and " + missing_ingredients[-1]
-            print(f"Sorry we are out of {format_missing_ingredents}")
-        elif len(missing_ingredients) == 1:
-            print(f"Sorry we are out of {missing_ingredients[0]}")
+        if state and len(missing_ingredients) == 0:
+            repeat = True
+            while repeat:
+                try:
+                    pennies = int(input("How many pennies? \n")) * 0.01
+                except ValueError:
+                    print("Please enter a valid number")
+                else:
+                    break
+                finally:
+                    repeat = True
+                    
+            while repeat:            
+                try:
+                    nickels = int(input("How many nickles? \n")) * 0.05
+                except ValueError:
+                    print("Please enter a valid number")
+                else:
+                    break
+                finally:
+                    repeat = True
+
+            while repeat:
+                try:
+                    dimes = int(input("How many dimes? \n")) * 0.1
+                except ValueError:
+                    print("Please enter a valid number")
+                else:
+                    break
+                finally:
+                    repeat = True
+
+            while repeat:
+                try:
+                    quarters =  int(input("How many quarters? \n")) * 0.25
+                except ValueError:
+                    print("Please enter a valid number")
+                else:
+                    break
+                finally:
+                    repeat = True
+
+            money_in, total, total_format, change = handle_payment(
+                                                    user_choice,
+                                                    quarters,
+                                                    dimes,
+                                                    nickels,
+                                                    pennies)
+            
+            if money_in and total == MENU[user_choice]["cost"]:
+                make_coffee(user_choice)
+                print(f"You inserted {total_format}. "
+                        f"Your {user_choice.title()} is ready! Enjoy!")
+            elif money_in and total > MENU[user_choice]["cost"]:
+                make_coffee(user_choice)
+                print(f"You inserted {total_format}. "
+                        f"You're {user_choice.title()} is ready! Enjoy!\n"
+                        f"Here's your change: ${change}")
+            elif not money_in:
+                print(f"You inserted {total_format}. "
+                    f"Sorry, {user_choice.title()} "
+                    f"costs ${MENU[user_choice]['cost']:.2f}\n"
+                    "Please try again")
+        elif user_choice in MENU and not state:
+            if len(missing_ingredients) > 1:
+                format_missing_ingredents = ", ".join(missing_ingredients[:-1]) + " and " + missing_ingredients[-1]
+                print(f"Sorry we are out of {format_missing_ingredents}")
+            elif len(missing_ingredients) == 1:
+                print(f"Sorry we are out of {missing_ingredients[0]}")
     elif user_choice not in MENU and user_choice != "off" and user_choice != "report":
         print("Please enter a valid option.")
 
@@ -109,9 +156,5 @@ while machine_on:
     # [ ] TODO add function to refill the machine perhaps this, off and report can 
     # only be handled by an admin with a password?
 
-    # [x] TODO Fix error handling for empty resources. When resource empty, else
+    # [ x ] TODO Fix error handling for empty resources. When resource empty, else
     # block runs.
-
-    # [x] TODO Fix bug with report and off command, returns KeyError: '{value}'
-    # It seems to be checking for off or report in check_resources meaning regardless of my precondition check, it's 
-    # bypassing and still running the main logic in the main loop.
